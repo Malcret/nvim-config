ce4d50b29b9fab3e2919ad620018c6875ab3bd29
Malcret <t.marcopoulos@outlook.fr>
2023-05-08 22:07:49 +0200

    add quit all keymap

--- lua/remap.lua
+++ lua/remap.lua
@@ -13,3 +13,5 @@ vim.keymap.set("n", "<leader>wh", "<C-w>h")
 vim.keymap.set("n", "<leader>wj", "<C-w>j")
 vim.keymap.set("n", "<leader>wk", "<C-w>k")
 vim.keymap.set("n", "<leader>wl", "<C-w>l")
+
+vim.keymap.set('n', '<leader>qq', ':quitall<CR>')
