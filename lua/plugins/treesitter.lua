2ad2ec2c599ea5ccec789ef47b288f10b1b0ed14
Théotime Marcopoulos <theotime.marcopoulos@malcret.com>
2024-04-07 19:25:15 +0200

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,69 +4,13 @@ return {
         'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
-    config = function ()
-        vim.defer_fn(function()
-            require('nvim-treesitter.configs').setup({
-                ensure_installed = { 'bash', 'c', 'cpp', 'lua', 'rust' },
-                auto_install = false,
-                sync_install = false,
-                ingore_install = {},
-                modules = {},
-                highlight = { enable = true },
-                indent = { enable = true },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = '<c-space>',
-                        node_incremental = '<c-space>',
-                        scope_incremental = '<c-s>',
-                        node_decremental = '<M-space>',
-                    },
-                },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ['aa'] = '@parameter.outer',
-                            ['ia'] = '@parameter.inner',
-                            ['af'] = '@function.outer',
-                            ['if'] = '@function.inner',
-                            ['ac'] = '@class.outer',
-                            ['ic'] = '@class.inner',
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true,
-                        goto_next_start = {
-                            [']m'] = '@function.outer',
-                            [']]'] = '@class.outer',
-                        },
-                        goto_next_end = {
-                            [']M'] = '@function.outer',
-                            [']['] = '@class.outer',
-                        },
-                        goto_previous_start = {
-                            ['[m'] = '@function.outer',
-                            ['[['] = '@class.outer',
-                        },
-                        goto_previous_end = {
-                            ['[M'] = '@function.outer',
-                            ['[]'] = '@class.outer',
-                        },
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ['<leader>a'] = '@parameter.inner',
-                        },
-                        swap_previous = {
-                            ['<leader>A'] = '@parameter.inner',
-                        },
-                    },
-                },
-            })
-        end, 0)
+    opts = {
+        ensure_installed = { 'bash', 'c', 'cpp', 'lua', 'luadoc', 'rust', 'vim', 'vimdoc' },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+    },
+    config = function(_, opts)
+        require('nvim-treesitter.configs').setup(opts)
     end
 }
